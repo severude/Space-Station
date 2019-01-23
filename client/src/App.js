e8553be9ecb989bbd74ec88603f871ad8b8d827a
Dale Severude <dseverude@msn.com>
2019-01-22 23:47:12 -0800

    Add map animation

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Grid, Jumbotron} from 'react-bootstrap';
-import L from 'leaflet';
+import { Map, TileLayer, Circle } from 'react-leaflet';
 
 class App extends Component {
 
@@ -15,6 +15,7 @@ class App extends Component {
       people: [],
       nextPassBy: null,
       duration: 0,
+      zoom: 6,
       loading: true
     };
   }
@@ -22,12 +23,7 @@ class App extends Component {
   componentDidMount() {
     this.myLocation();
 
-    this.callBackendAPI('/location')
-      .then(res => {
-        this.setState({ lat: res.latitude, lon: res.longitude })
-        this.renderMap();
-      })
-      .catch(err => console.log(err));
+    this.interval();
 
     this.callBackendAPI('/people')
       .then(res => {
@@ -51,6 +47,16 @@ class App extends Component {
     return body;
   };
 
+  interval() {
+    this.callBackendAPI('/location')
+    .then(res => {
+      this.setState({ lat: res.latitude, lon: res.longitude, loading: false })
+    })
+    .catch(err => console.log(err));
+
+    setTimeout(this.interval.bind(this), 3000);
+  }
+
   getPosition = (options = {}) => {
     return new Promise(function (resolve, reject) {
       navigator.geolocation.getCurrentPosition(resolve, reject, options);
@@ -63,8 +69,7 @@ class App extends Component {
       let position = pos.coords;
       this.setState({
         my_lat: position.latitude.toFixed(4),
-        my_lon: position.longitude.toFixed(4),
-        loading: false
+        my_lon: position.longitude.toFixed(4)
       });
       this.callBackendAPI(`/nextPassBy/${this.state.my_lat}/${this.state.my_lon}`)
         .then(res => {
@@ -77,27 +82,6 @@ class App extends Component {
       console.warn(`${err.code}: ${err.message}`);
     }
   }
-
-  renderMap = () => {
-    let mymap = L.map('mapid');
-    mymap.setView([this.state.lat, this.state.lon], 6);
-
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-      maxZoom: 18,
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      id: 'mapbox.streets',
-      accessToken: 'pk.eyJ1IjoiZHNldmVydWRlIiwiYSI6ImNqcjg1bjJ3djAzYXk0M253anlueXBsMWgifQ.K1U8coDnDF_hpCrLPvrbqA'
-    }).addTo(mymap);
-
-    L.circle([this.state.lat, this.state.lon], {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5,
-        radius: 500
-    }).addTo(mymap);
-}
 
   render() {
     return (
@@ -114,7 +98,25 @@ class App extends Component {
               ? <h3>The ISS passes by your current location {this.state.my_lat} {this.state.my_lon} on {this.state.nextPassBy} for {this.state.duration} minutes.</h3>
               : <h3>The ISS does not pass by your current location: {this.state.my_lat} {this.state.my_lon}</h3>
             }
-            <div id="mapid"></div>
+            {
+              (this.state.loading)
+                ? <p>Loading...</p>
+                : <Map center={[this.state.lat, this.state.lon]} zoom={this.state.zoom} >
+                    <TileLayer 
+                      url='https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}'
+                      maxZoom={18}
+                      attribution= 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+                      accessToken= 'pk.eyJ1IjoiZHNldmVydWRlIiwiYSI6ImNqcjg1bjJ3djAzYXk0M253anlueXBsMWgifQ.K1U8coDnDF_hpCrLPvrbqA'
+                      id= 'mapbox.streets'
+                    />
+                    <Circle center={[this.state.lat, this.state.lon]}
+                      color='red'
+                      fillColor='#f03'
+                      fillOpacity={0.5}
+                      radius={500}
+                    />
+                  </Map>
+            }
           </Grid>
         </Jumbotron>
       </div>
