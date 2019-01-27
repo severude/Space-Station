44abd995a06af18a9bc4d800015fbbeb5a43c4bf
Dale Severude <dseverude@msn.com>
2019-01-26 16:09:09 -0800

    Mobile responsive

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Jumbotron, Badge } from 'react-bootstrap';
+import { Grid, Row, Col, Jumbotron, Badge, Well } from 'react-bootstrap';
 import { Map, TileLayer, Circle } from 'react-leaflet';
 
 class App extends Component {
@@ -137,63 +137,78 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Jumbotron>
+          <Jumbotron className=" bg-info text-white">
+            <h1 className="display-2 text-center">International Space Station Locator</h1>
+          </Jumbotron>
           <Grid>
-            <h1 className="display-2">Tracking the International Space Station (ISS)</h1>
-            <p>The IIS travels at an altitude of about 250 miles above the earth and at a speed of about 17,100 miles per hour.  That is about 5 miles per second.  It orbits the earth every 92 minutes.</p>
-          </Grid>
-        </Jumbotron>
-          <Grid>
-            {
-              (this.state.lat)
-                ? <h2 className="text-center"><Badge className="p-2">{this.state.lat}</Badge> latitude <Badge className="p-2">{this.state.lon}</Badge> longitude</h2>
-                : <p></p>
-            }
-          </Grid>
-            {
-              (this.state.loading)
-                ? <h2 className="text-center">Loading...</h2>
-                : <Map className="map-container" center={[this.state.lat, this.state.lon]} 
-                      ref={(ref) => { this.map = ref; }}
-                      zoom={this.state.zoom} >
-                    <TileLayer 
-                      url='https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}'
-                      maxZoom={18}
-                      attribution= 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
-                      accessToken= 'pk.eyJ1IjoiZHNldmVydWRlIiwiYSI6ImNqcjg1bjJ3djAzYXk0M253anlueXBsMWgifQ.K1U8coDnDF_hpCrLPvrbqA'
-                      id= 'mapbox.streets'
-                    />
-                    <Circle center={[this.state.lat, this.state.lon]}
-                      color='red'
-                      fillColor='#f03'
-                      fillOpacity={0.5}
-                      radius={500}
-                    />
-                  </Map>
-            }
-            {
-              (this.state.icon) 
-              ? <img className="weather-image" src={`https://openweathermap.org/img/w/${this.state.icon}.png`} alt="weather icon" />
-              : <p></p>
-            }
-            {
-              (this.state.status)
-              ? <h3 className="text-center">Conditions: {this.state.status} {this.state.temperature}&#176;</h3>
-              : <p></p>
-            }
-            {
-              (this.state.direction)
-              ? <h3 className="text-center">Wind: {this.state.direction} {this.state.wind} mph</h3>
-              : <p></p>
-            }
-            <h3 className="text-center">{this.state.area}</h3>
-            <p className="text-center">The following {this.state.travelers} people are currently on board the ISS:</p>
-            <p className="text-center">{this.state.people.map((person, index) => <li key={index}>{person}</li>)}</p>
-            {
-              (this.state.duration)
-              ? <p className="text-center">The ISS passes by your current location {this.state.my_lat} {this.state.my_lon} on {this.state.nextPassBy} for {this.state.duration} minutes.</p>
-              : <p className="text-center">The ISS does not pass by your current location: {this.state.my_lat} {this.state.my_lon}</p>
-            }
+            <Row>
+              <Col className="col-lg-8" >
+                {
+                  (this.state.lat)
+                    ? <h2 className="text-center"><Badge className="p-2">{this.state.lat}</Badge> latitude <Badge className="p-2">{this.state.lon}</Badge> longitude</h2>
+                    : <p></p>
+                }
+                {
+                  (this.state.loading)
+                    ? <h2 className="text-center">Loading...</h2>
+                    : <Map className="map-container" center={[this.state.lat, this.state.lon]} 
+                          ref={(ref) => { this.map = ref; }}
+                          zoom={this.state.zoom} >
+                        <TileLayer 
+                          url='https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}'
+                          maxZoom={18}
+                          attribution= 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+                          accessToken= 'pk.eyJ1IjoiZHNldmVydWRlIiwiYSI6ImNqcjg1bjJ3djAzYXk0M253anlueXBsMWgifQ.K1U8coDnDF_hpCrLPvrbqA'
+                          id= 'mapbox.streets'
+                        />
+                        <Circle center={[this.state.lat, this.state.lon]}
+                          color='red'
+                          fillColor='#f03'
+                          fillOpacity={0.5}
+                          radius={500}
+                        />
+                      </Map>
+                }
+                <Row className="mx-auto">
+                    <Col xsOffset={0}>
+                      {
+                        (this.state.icon) 
+                        ? <img src={`https://openweathermap.org/img/w/${this.state.icon}.png`} alt="weather icon" />
+                        : <p></p>
+                      }
+                    </Col>
+                    <Col>
+                      {
+                        (this.state.status)
+                        ? <h3 className="text-center pt-3"> {this.state.status} {this.state.temperature}&#176;</h3>
+                        : <p></p>
+                      }
+                    </Col>
+                    <Col>
+                      {
+                        (this.state.direction)
+                        ? <h3 className="text-center pt-3">{this.state.direction} wind {this.state.wind} mph</h3>
+                        : <p></p>
+                      }
+                    </Col>
+                </Row>
+                <h1 className="text-center mb-5">{this.state.area}</h1>
+              </Col>
+              <Col className="col-lg" >
+                <Well bsSize="large"><h3 className="text-muted">The ISS travels at an altitude of about 250 miles above the earth and at a speed of about 17,100 miles per hour.  That is about 5 miles per second.  It orbits the earth every 92 minutes.</h3></Well>
+                {
+                  (this.state.duration)
+                  ? <Well bsSize="large"><h3 className="text-muted">The ISS will pass by your current location of <Badge className="p-2">{this.state.my_lat}</Badge>, <Badge className="p-2">{this.state.my_lon}</Badge> on {this.state.nextPassBy} for {this.state.duration} minutes.</h3></Well>
+                  : <Well bsSize="large"><h3 className="text-muted">The ISS does not pass by your current location: <Badge className="p-2">{this.state.my_lat}</Badge> <Badge className="p-2">{this.state.my_lon}</Badge></h3></Well>
+                }
+                <Well bsSize="large">
+                  <h3 className="text-muted">The following {this.state.travelers} people are currently on board the ISS:</h3>
+                  <h3 className="text-muted">{this.state.people.map((person, index) => <li key={index}>{person}</li>)}</h3>
+                </Well>
+              </Col>
+            </Row>
+
+        </Grid>
       </div>
     ); // end render return
   } // end render
